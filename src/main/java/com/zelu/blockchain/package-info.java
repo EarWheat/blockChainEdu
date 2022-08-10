414d64693196f764b333a2e81fb175dd5ce4420f
liuzhaolu.lzl <liuzhaolu.lzl@alibaba-inc.com>
2022-08-10 17:26:09 +0800

    区块链教学

---
+++ src/main/java/com/zelu/blockchain/package-info.java
@@ -0,0 +1,1 @@
+package com.zelu.blockchain;
---
+++ src/main/java/com/zelu/blockchain/wallet/WalletUtil.java
@@ -0,0 +1,10 @@
+package com.zelu.blockchain.wallet;
+
+/**
+ * @Desc: 钱包工具类
+ * @Author: 泽露
+ * @Date: 2022/8/10 5:25 PM
+ * @Version: 1.initial version; 2022/8/10 5:25 PM
+ */
+public class WalletUtil {
+}
